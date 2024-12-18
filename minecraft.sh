8ebd3c05fc452e2d6b064c34ad85b9c7fe68bcfd
pachun04 <pachun04@gmail.com>
2024-12-18 23:32:28 +0000

    save_success

--- minecraft.sh
+++ minecraft.sh
@@ -3,13 +3,13 @@
 eval "$(ssh-agent -s)"  # Inicia el agente SSH
 ssh-add ~/.ssh/id_ed25519  # Carga la clave SSH (asegúrate de que esta es tu clave privada correcta)
 
+# Inicia el servidor de Minecraft en segundo plano
+screen -dmS minecraft java -Xms128M -Xmx1024M -jar server.jar nogui
+
 # Realiza back up del servidor
 git add .
 
 git commit -m "save_success"
 
 git push
-
-# Inicia el servidor de Minecraft en segundo plano
-screen -dmS minecraft java -Xms128M -Xmx1024M -jar server.jar nogui
 
