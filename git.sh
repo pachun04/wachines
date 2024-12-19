d6ac5092108552c5d67182af19b2bc14aee43feb
pachun04 <pachun04@gmail.com>
2024-12-19 00:15:46 +0000

    save_success

---
+++ git.sh
@@ -0,0 +1,5 @@
+#~/bin/bash
+
+git add .
+git commit -m "save_success"
+git push
--- minecraft.sh
+++ minecraft.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-java -Xms128M -Xmx1024M -jar server.jar nogui &
+nohup java -Xms128M -Xmx1024M -jar server.jar nogui &
 
-git add .
-git commit -m "save_success"
-git push
+wait
+
+./git.sh
